67c5dee546fdd2aca585cc71367a6b86794f88db
hhlee <lonerjoker@gmail.com>
2025-04-08 11:24:31 +0900

    Create sidebars.js

---
+++ docs/sidebars.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tutorialSidebar: [
+    'intro',
+    {
+      type: 'category',
+      label: '기능 정의서',
+      items: ['features/invite-user'],
+    },
+    {
+      type: 'category',
+      label: 'API 명세',
+      items: ['api/v1'],
+    },
+  ],
+};
